2f06a4d96a41c0ef0eac1453a7f9d2f1b3acc13c
Kevin Johnson <kevinejohn@gmail.com>
2011-08-05 11:27:03 -0500

    Initial Commit

---
+++ CompanyEmployees/CompanyEmployeesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CompanyEmployeesAppDelegate.h
+//  CompanyEmployees
+//
+//  Created by Kevin Johnson on 8/5/11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompanyEmployeesAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
